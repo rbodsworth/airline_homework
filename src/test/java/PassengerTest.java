257f38d2c355a0f5e4ef9420ba4f08678288d269
Richard Bodsworth <richardbodsworth@gmail.com>
2021-02-13 17:25:47 +0000

    passenger tests passing

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+
+    Passenger passenger;
+
+    @Before
+    public void before(){
+        passenger = new Passenger("John Cutter", 1);
+    }
+
+    @Test
+    public void canGetPassengerName(){
+        assertEquals("John Cutter", passenger.getName());
+    }
+
+    @Test
+    public void canGetNumberOfBags(){
+        assertEquals(1, passenger.getNumberOfBags());
+    }
+
+
 }
